363094c3baf1d20b5b8773e2a3c0838811820559
Kyriios188 <robin.siim188@gmail.com>
2022-12-16 11:56:05 +0100

    refactored to reduces lines

--- metadata_server/webapp.py
+++ metadata_server/webapp.py
@@ -1,74 +1,18 @@
 import json
 import uuid
-import requests
 import os
 from flask import Flask, request
 
 webapp = Flask(__name__)
 
 
-@webapp.route('/srv')
-def srv():
+@webapp.route('/metadata/<str:ct_type>')
+def metadata(ct_type: str):
    with open('metadata.json', 'r') as j:
       metadata = json.loads(j.read())
    
-   return(str(metadata['srv']))
+   return(str(metadata[ct_type]))
 
-@webapp.route('/gwi')
-def gwi():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['gwi']))
-
-@webapp.route('/gwf1')
-def gwf1():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['gwf1']))
-
-@webapp.route('/gwf2')
-def gwf2():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['gwf2']))
-
-@webapp.route('/gwf3')
-def gwf3():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['gwf3']))
-
-@webapp.route('/dev1')
-def dev1():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['dev1']))
-
-@webapp.route('/dev2')
-def dev2():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['dev2']))
-
-@webapp.route('/dev3')
-def dev3():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['dev3']))
-
-@webapp.route('/app')
-def app():
-   with open('metadata.json', 'r') as j:
-      metadata = json.loads(j.read())
-   
-   return(str(metadata['app']))
 
 @webapp.route('/ip', methods=['GET'])
 def ip():
