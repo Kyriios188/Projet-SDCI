f2e06fb4e9c72df476fe7cacc49d6e649b4a7bed
Kyriios188 <robin.siim188@gmail.com>
2023-01-25 14:07:37 +0100

    fixing automatic VNF deployment of monitor VNF

--- src/main/java/Execute.java
+++ src/main/java/Execute.java
@@ -79,7 +79,7 @@ class Execute {
     public static void deploy_monitoring_vnf() {
         manoapi.deploy_monitoring_vnf();
         try {
-            Shell.executeCmd("curl -s -X GET http://10.0.0.20:5001/monitor");
+            Shell.executeCmd("curl -s -X GET http://127.0.0.1:32769/monitor");
         } catch (IOException e) {
             e.printStackTrace();
         }
