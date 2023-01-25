190bc66fa7685a519488b775231a9dd4da4044cb
Kyriios188 <robin.siim188@gmail.com>
2023-01-25 13:27:09 +0100

    added Shell class to manage Shell commands

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -8,23 +8,35 @@ public class Shell {
 
         Process process = Runtime.getRuntime().exec(cmd);
 
+        printCmdOutput(process);
+        printCmdErrors(process);
+
+    }
+
+    static private void printCmdOutput(Process process) throws IOException {
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(process.getInputStream()));
+        // Read the output from the command
+        String s;
 
+        System.out.println("\033[0;33m");
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println("[Shell OUTPUT] : " + s);
+        }
+        System.out.println("\033[0m");
+    }
+
+    static private void printCmdErrors(Process process) throws IOException {
+        String s;
         BufferedReader stdError = new BufferedReader(new
                 InputStreamReader(process.getErrorStream()));
 
-        // Read the output from the command
-        System.out.println("Here is the standard output of the command:\n");
-        String s;
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
-
+        System.out.println("\033[0;31m\n");
         // Read any errors from the attempted command
         System.out.println("Here is the standard error of the command (if any):\n");
         while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
+            System.out.println("[Shell ERROR] : " + s);
         }
+        System.out.println("\033[0m");
     }
 }
