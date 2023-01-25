0a34585ee3ccc8d2dee40856a6c0393071e3320b
Kyriios188 <robin.siim188@gmail.com>
2023-01-25 13:41:54 +0100

    the new name of the monitoring vnf interface was too long, renamed again + removed some prints

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -33,7 +33,6 @@ public class Shell {
 
         System.out.println("\033[0;31m\n");
         // Read any errors from the attempted command
-        System.out.println("Here is the standard error of the command (if any):\n");
         while ((s = stdError.readLine()) != null) {
             System.out.println("[Shell ERROR] : " + s);
         }
